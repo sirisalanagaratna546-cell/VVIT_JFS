cee8902ddb2d2be5aca9fee0448c242682db545d
vvit <sirisalanagaratna546@gmail.com>
2026-08-27 12:30:49 +0530

    Update p1.java

---
+++ Day 1/p1.java/p1.java
@@ -0,0 +1,15 @@
+// Variables in Java
+public class P1 {
+    public static void main(String[] args) {
+        short shortnumber = 32000;
+        int age = 25;
+
+        float temperature = 36.6f;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isEnrolled = true;
+
+        String studentName = "Rakesh";
+        System.out.println("Student: " + studentName);
+    }
+}
